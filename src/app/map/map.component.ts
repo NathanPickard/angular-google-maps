f75cf1be705e03c4863b99a40e4746da9f261033
Nathan Pickard <nathan.pickard87@gmail.com>
2021-06-11 18:08:02 -0700

    Added map options to google map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,6 +11,14 @@ import { catchError, map } from 'rxjs/operators';
 export class MapComponent implements OnInit {
 
   apiLoaded: Observable<boolean>;
+  zoom = 12;
+
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
 
   constructor(httpClient: HttpClient) {
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyC2GzgTEHS2Dt3A541ar4Li358M3ILMOPk', 'callback')

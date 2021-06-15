4b08f91d5c873e563aebfa23e892c0b2baefe703
Nathan Pickard <nathan.pickard87@gmail.com>
2021-06-14 20:23:14 -0700

    Map markers on map click

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,14 +12,18 @@ export class MapComponent implements OnInit {
 
   apiLoaded: Observable<boolean>;
   zoom = 12;
-  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+  // center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
 
   options: google.maps.MapOptions = {
-    mapTypeId: 'hybrid',
+    center: { lat: 45.5212, lng: -122.664 },
+    mapTypeId: 'roadmap',
     disableDoubleClickZoom: true,
     maxZoom: 15,
     minZoom: 8,
   }
+
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
   constructor(httpClient: HttpClient) {
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyC2GzgTEHS2Dt3A541ar4Li358M3ILMOPk', 'callback')
@@ -30,12 +34,16 @@ export class MapComponent implements OnInit {
   }
 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-    })
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //   this.center = {
+    //     lat: position.coords.latitude,
+    //     lng: position.coords.longitude,
+    //   }
+    // })
+  }
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    this.markerPositions.push(event.latLng.toJSON());
   }
 
 }
